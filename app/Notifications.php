6618b0fd09ce0f5935e9c89218ece6d39c7f3f5c
Nikhil Srivastava <srivastnikhil@gmail.com>
2015-02-09 01:16:04 +0530

    created model for notificatins table

---
+++ app/Notifications.php
@@ -0,0 +1,9 @@
+<?php namespace shyfirst;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notifications extends Model {
+	 protected $table = 'notifications';	
+	 public $timestamps = false;
+
+}
