0ae1730b1ae24e3addc680338fae4930c6b972f4
Nikhil Srivastava <srivastnikhil@gmail.com>
2015-02-09 05:30:44 +0530

    getAttr function to get visibile fields for a user

--- app/Access.php
+++ app/Access.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Access extends Model {
      protected $table = 'access';	
 	 public $timestamps = false;
+
+	 public static function getAttr($desperate,$cool){
+	 	$temp = Access::where('desperate','=',$desperate)->first();
+	 	return $temp->attr;
+	 }
 }
