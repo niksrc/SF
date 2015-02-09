cba1a3824a5755be6437f64582c133db634e9cca
Nikhil Srivastava <srivastnikhil@gmail.com>
2015-02-09 23:25:14 +0530

    getAccess checks the status between two person

--- app/Access.php
+++ app/Access.php
@@ -6,8 +6,14 @@ class Access extends Model {
      protected $table = 'access';	
 	 public $timestamps = false;
 
-	 public static function getAttr($desperate,$cool){
-	 	$temp = Access::where('desperate','=',$desperate)->first();
-	 	return $temp->attr;
+	 public static function getStatus($desperate,$cool){
+	 	$temp1 = Access::where('desperate','=',$desperate,'and','cool','=',$cool)->first();
+	 	$temp2 = Access::where('desperate','=',$cool,'and','cool','=',$desperate)->first();
+	 
+		if($temp1&&$temp2){
+		 	return true;
+		}else
+		 	return false;
 	 }
+	 
 }
